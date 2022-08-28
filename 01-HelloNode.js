86fc78060b12eb0f6a49d36ddc6ca5f5b16392ac
이.경.재 <53504037+rudwo4@users.noreply.github.com>
2022-08-28 12:05:34 +0900

    (1주차) Node.js 강의
    2022_08_28 교육

---
+++ 01-HelloNode.js
@@ -0,0 +1,13 @@
+console.group("First"); //로그를 그룹단위 폴더 처리
+
+console.log("Hello World"); // 변수 값을 확인하기 위한 용도 사용
+
+console.debug("Hello World"); 
+
+console.info("Hello World");
+
+console.warn("Hello World");
+
+console.error("Hello World");
+
+console.groupEnd
---
+++ 02-env.js
@@ -0,0 +1,3 @@
+//process.env.환경변수이름
+console.log(process.env.NODE_MSG);
+console.log(process.env.PATH);
